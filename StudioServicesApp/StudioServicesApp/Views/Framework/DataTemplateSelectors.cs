f3d78d6116b1b9432ef6ce8ba4c18c3e78b20704
pinoelefante <pinoelefante@hotmail.it>
2018-03-15 16:16:38 +0100

    news template

--- StudioServicesApp/StudioServicesApp/Views/Framework/DataTemplateSelectors.cs
+++ StudioServicesApp/StudioServicesApp/Views/Framework/DataTemplateSelectors.cs
@@ -3,9 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudioServices.Data.Newsboard;
 using Xamarin.Forms;
 
 namespace StudioServicesApp.Views
 {
-    
+	public class NewsboardNewsTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate NewsReaded { get; set; }
+		public DataTemplate NewsNotRead { get; set; }
+
+		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+		{
+			var message = item as Message;
+			if (message == null)
+				return null;
+			if (message.IsRead)
+				return NewsReaded;
+			return NewsNotRead;
+		}
+	}
 }
