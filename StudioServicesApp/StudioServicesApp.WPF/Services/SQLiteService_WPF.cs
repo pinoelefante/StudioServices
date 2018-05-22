49b1ea096b60f7bd96266f9d8f643d0e06686200
pinoelefante <pinoelefante@hotmail.it>
2018-05-22 02:08:04 +0200

    - xamarin forms 3.1 - wpf project

---
+++ StudioServicesApp/StudioServicesApp.WPF/Services/SQLiteService_WPF.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Xamarin.Forms;
+using System.IO;
+using pinoelefante.Services;
+using pinoelefante.UWP.Service;
+using System.Reflection;
+
+[assembly: Dependency(typeof(SQLiteService_WPF))]
+namespace pinoelefante.UWP.Service
+{
+    public class SQLiteService_WPF : ISQLite
+    {
+        public SQLiteService_WPF()
+        {
+            var baseFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            DATABASE = Path.Combine(baseFolder, "db.sqlite");
+        }
+        private static string DATABASE;
+        public SQLiteConnection GetConnection()
+        {
+            var conn = new SQLiteConnection(DATABASE);
+            return conn;
+        }
+    }
+}
